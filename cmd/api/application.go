fca162c836726f9cd7e5a3d908de6431a0943d35
Akbar Sena Wijaya <120768034+akbarsenawjy26@users.noreply.github.com>
2024-09-09 10:15:05 +0700

    feat(main): update main program

--- cmd/api/application.go
+++ cmd/api/application.go
@@ -5,9 +5,13 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"task/app"
 	"task/common/helper"
+	"task/common/jwt"
 	"task/src/repository"
 	"task/src/task/application"
+
 	"task/src/task/controller"
+	application2 "task/src/users/application"
+	controller2 "task/src/users/controller"
 )
 
 func main() {
@@ -19,8 +23,11 @@ func main() {
 	queries := repository.New(database)
 	tx := helper.NewTransactionHelper(database)
 	taskService := application.NewTaskServiceImpl(queries, tx)
+	tokenJWT := jwt.NewJWTServiceImpl()
+	userService := application2.NewUserServiceImpl(queries, tx, tokenJWT)
 	var taskController controller.TaskController = controller.NewTaskControllerImpl(taskService)
-	RegisterRoutes(e, taskController)
+	var userController controller2.UserController = controller2.NewUserControllerImpl(userService)
+	RegisterRoutes(e, taskController, userController)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
